e7a9940bb3137744e8c188c24b58cad3a157a0ff
joaovictor0123mo@gmail.com <joaovictor0123mo@gmail.com>
2026-08-11 00:37:37 -0300

    Add Department entity

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,12 +1,10 @@
 package application;
 
-import db.DB;
-
-import java.sql.Connection;
+import model.entities.Department;
 
 public class Program {
     public static void main(String[] args){
-        Connection connection = DB.getConnection();
-        DB.closeConnection();
+        Department department = new Department(1, "Books");
+        System.out.println(department);
     }
 }
