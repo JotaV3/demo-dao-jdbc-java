c8d63f25e9dd2ae5a149e7d015a0ee4e0a0cac2e
joaovictor0123mo@gmail.com <joaovictor0123mo@gmail.com>
2026-08-13 20:29:07 -0300

    Implement Seller update

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -2,20 +2,18 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.dao.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
-import java.util.List;
 
 public class Program {
     public static void main(String[] args){
-        Department department = new Department(3, null);
-        Seller seller = new Seller(null, "Vitoria", "vitoria@gmail.com", new Date(), 1500, department);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        sellerDao.insert(seller);
+        Seller seller = sellerDao.findById(1);
+        seller.setName("Bob Philips");
+
+        sellerDao.update(seller);
 
         System.out.println(seller);
     }
