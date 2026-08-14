80e84eee888978d82721f1dfff8e76eb0c3f4773
joaovictor0123mo@gmail.com <joaovictor0123mo@gmail.com>
2026-08-13 22:15:43 -0300

    Initialize DepartmentDaoJDBC

--- src/main/java/model/dao/DaoFactory.java
+++ src/main/java/model/dao/DaoFactory.java
@@ -8,6 +8,6 @@ public class DaoFactory {
     }
 
     public static DepartmentDao createDepartmentDao(){
-        return new DepartmentDaoJDBC();
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 }
--- src/main/java/model/dao/DepartmentDaoJDBC.java
+++ src/main/java/model/dao/DepartmentDaoJDBC.java
@@ -2,9 +2,16 @@ package model.dao;
 
 import model.entities.Department;
 
+import java.sql.Connection;
 import java.util.List;
 
 public class DepartmentDaoJDBC implements DepartmentDao{
+    private Connection connection;
+
+    public DepartmentDaoJDBC(Connection connection){
+        this.connection = connection;
+    }
+
     @Override
     public void insert(Department department) {
 
