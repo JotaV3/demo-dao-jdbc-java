b0a4f42146d0251b98ffda474b9f9d6354e554e2
joaovictor0123mo@gmail.com <joaovictor0123mo@gmail.com>
2026-08-13 22:42:36 -0300

    Implement Department update

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -7,9 +7,9 @@ import model.entities.Department;
 public class Program {
     public static void main(String[] args){
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
-        Department department = new Department(null, "Games");
+        Department department = new Department(3, "Clothes");
 
-        departmentDao.insert(department);
+        departmentDao.update(department);
 
         System.out.println(department);
     }
